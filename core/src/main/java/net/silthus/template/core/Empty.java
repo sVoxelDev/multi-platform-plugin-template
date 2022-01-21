c01f9b8b1fcf5361ac466b5e99bc025f55e75a17
Reichenbach, Michael <michael.reichenbach@interhyp.de>
2022-01-21 08:27:15 +0100

    feat: add basic core module structure

---
+++ core/src/main/java/net/silthus/template/core/Empty.java
@@ -0,0 +1,6 @@
+package net.silthus.template.core;
+
+public class Empty {
+
+    // TODO: add your use-cases and business rules into core
+}
---
+++ core/src/test/java/net/silthus/template/core/CoreTests.java
@@ -0,0 +1,11 @@
+package net.silthus.template.core;
+
+import org.junit.jupiter.api.Test;
+
+class CoreTests {
+
+    @Test
+    void given_empty_core_then_tests_are_executable() {
+        // TODO: add your use-cases and business rules into core
+    }
+}
